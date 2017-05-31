f0930342bf093ddf792c7753ce807ee8cb0d6601
Anton Yashchuk <anton.yashchuk@gmail.com>
2017-05-31 14:52:34 +0000

    setting up mongo(ose) at serverside

--- src/server.js
+++ src/server.js
@@ -1,53 +1,69 @@
 
-var data = {
-  test1: {
-    test11:"foo",
-    test3:"bar"
-  },
-  test2: 123,
-  test3: "bar"
-};
 
-function getData(){
-  console.log("get data: "+JSON.stringify(data));
-  return JSON.stringify(data);
-}
-
-function setData(d){
-  if (typeof d !== 'string') d=JSON.stringify(d);
-  console.log("set data: "+d);
-  data = JSON.parse(d);
-  return d;
-}
-  
 var express = require('express')
   , url = require("url")
   , bodyParser = require("body-parser")
   , jsonParser = bodyParser.json()
   , app = express()
-  , expressWs = require("express-ws")(app);
+  , expressWs = require("express-ws")(app)
+  , dbconfig = require('./dbconf.json')
+  , mongoose = require('mongoose');
+
+mongoose.connect('mongodb://'+dbconfig.username+':'+dbconfig.password+'@ds157631.mlab.com:57631/seamless-test')
+  .catch(function(err){throw err});
+  
+var Schema = mongoose.Schema;
+var TestSchema = new Schema({
+  type: String,
+  count: Number,
+  hoverable: Boolean,
+  message: String,
+  addresee: String
+});
+TestSchema.index({addresee:1,type:1});
+
+TestSchema.post('save',function(doc){
+  
+})
+
+
+var TestModel = mongoose.model('TestModel',TestSchema);
+
+function TestData(){
+  var buffer,flag;
+  function setData(data){
+    
+  }
+  function getData(){
+    
+  }
+  return {
+    setData,
+    getData
+  }
+}
+
+var Test = TestData();
 
 app.use(require("helmet")());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/test', jsonParser, function (req, res) {
-  res.send(getData());
+  res.send(Test.getData());
 });
 app.post('/test', jsonParser, function(req, res){
   if (!req.body) res.sendStatus(400);
-  setData(req.body);
-  res.json(req.body);
+  else res.json(Test.setData(req.body));
 });
 
 app.ws('/test', function(ws, req){
   var location = url.parse(ws.upgradeReq.url, true);
   ws.on('message', function incoming(message,flags) {
     if (!flags.binary){
-      setData(message);
-      ws.send(getData());
+      ws.send(Test.setData(message));
     }
   });
-  ws.send(getData());
+  ws.send(Test.getData());
 });
 
 app.use(express.static('../tests/',{maxAge:1000}));
